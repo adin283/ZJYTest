764774740ea180583dcb4a8560be3fa5b90ea72c
祝文博 <zhuwenbo@zhujia360.com>
2017-11-14 16:01:31 +0800

    Initial Commit

---
+++ ZJYTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJYTest
+//
+//  Created by Kevin on 2017/11/14.
+//  Copyright © 2017年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJYTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJYTest
+//
+//  Created by Kevin on 2017/11/14.
+//  Copyright © 2017年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
